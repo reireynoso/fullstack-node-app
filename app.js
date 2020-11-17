299a6c1ee239895bbbfc57aff4bbbaff65b1bbd9
reireynoso <reireynoso@gmail.com>
2020-11-17 12:33:17 -0500

    edit storybook

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require("method-override")
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -19,22 +20,34 @@ connectDB()
 
 //intialize app
 const app = express()
+
 // body parser middleware
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+// method override 
+app.use(methodOverride(function(req,res){
+    if(req.body && typeof req.body === 'object' && '_method' in req.body){
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
 
 if(process.env.NODE_ENV === "development"){ // only want to run in dev mode
     app.use(morgan('dev')) // when there's a request to the page of any kind, is shows down in the console
 }
 
 // handlebards helpers
-const {formatDate, stripTags, truncate} = require('./helpers/hbs')
+const {formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
 
 // handlebars
 app.engine('.hbs', exphbs({helpers: {
     formatDate,
     stripTags,
-    truncate
+    truncate,
+    editIcon,
+    select
 }, defaultLayout:'main',extname: '.hbs'})); // setting up to replace using .handlebars extension to take in .hbs
 app.set('view engine', '.hbs');
 
@@ -51,6 +64,13 @@ app.use(session({
 // passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// set global var
+app.use(function(req,res,next){
+    // have access to user since it requires to login
+    res.locals.user = req.user || null 
+    next()
+})
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public'))) // provide the path where teh static folder is located
